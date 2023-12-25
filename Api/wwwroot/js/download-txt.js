8726fab79a2a32bb01b832e1a2b1e01b43879b01
Nahlenko Oleksandr <noapioss@gmail.com>
2023-12-26 00:16:20 +0200

    statisticcs filename

--- Api/wwwroot/js/download-txt.js
+++ Api/wwwroot/js/download-txt.js
@@ -1,18 +1,32 @@
 function downloadStatistics(id)
 {
     fetch(`/api/departments/statistics/${id}`)
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok ' + response.status);
-        }
-        return response.text();
-      })
-      .then(data => {
-        downloadTxtFile(data, "statisctics.txt");
-      })
-      .catch(error => {
-        console.error('There was a problem with the fetch operation:', error);
-      });
+        .then(response =>
+        {
+            if (!response.ok)
+            {
+            throw new Error('Network response was not ok ' + response.status);
+            }
+            
+            const contentDisposition = response.headers.get('Content-Disposition');
+            let filename = 'statistics.txt';
+            
+            if (contentDisposition && contentDisposition.indexOf('attachment') !== -1)
+            {
+                const filenameRegex = /filename[^;=\n]*=((['"]).*?\2|[^;\n]*)/;
+                const matches = filenameRegex.exec(contentDisposition);
+                if (matches != null && matches[1])
+                {
+                    filename = matches[1].replace(/['"]/g, '');
+                }
+            }
+
+            return downloadTxtFile(response.blob(), filename);
+        })
+        .catch(error =>
+        {
+            console.error('There was a problem with the fetch operation:', error);
+        });
 }
 
 function downloadTxtFile(data, filename)
